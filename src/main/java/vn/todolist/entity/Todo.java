f96d0a06bdb6d10bdf680e3c8b7445addd05abc3
DatTran <trandat5079@gmail.com>
2026-07-06 13:59:42 +0700

    feat: setup H2 database and create Todo entity

---
+++ src/main/java/vn/todolist/entity/Todo.java
@@ -0,0 +1,31 @@
+package vn.todolist.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "todos")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed = false;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
